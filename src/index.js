66fa4b323e3be47f05dcb00ca566ad72d9f6e20e
Ludwing <ludwing238@gmail.com>
2021-02-17 00:07:08 -0600

    integrate loader to handle css and style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import {start} from "./app/app";
-
+import "./css/style.css"
 start();
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,18 @@ const path = require("path");
 // Archivo de configuracion de webpack
 module.exports = {
   mode: "development", // development  (nomin), prod(min)
-  devtool:"none", //nos permite ver de mejor manera el codigo en nuestro output
+  devtool: false, //nos permite ver de mejor manera el codigo en nuestro output
   entry: "./src/index.js", // ubicacion de nuestro codigo fuente (donde inicia todo)
   output: {
     filename: "main.js", // nombre del archivo que deseamos generar para nuestro bundle
     path: path.resolve(__dirname, "dist"), //almacenamos nuestro output en un nuevo directorio dentro de nuestro root path
   },
-  //plugin
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // definimos el tipo de archivo para nuestra regla
+        use: ["style-loader","css-loader"], //utilizamos un loader para poder crear el bundle, tienen un orden
+      },
+    ],
+  },
 };
