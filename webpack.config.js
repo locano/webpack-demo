3ef93f203a0b82b24a9a5f9d3f11bd04b76472ab
Ludwing <ludwing238@gmail.com>
2021-02-17 00:27:22 -0600

    adding min css, extract it.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // tomamos el directorio actual (root path) de nuestro proyecto
 const path = require("path");
-
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // Archivo de configuracion de webpack
 module.exports = {
   mode: "development", // development  (nomin), prod(min)
@@ -10,12 +11,13 @@ module.exports = {
     filename: "main.js", // nombre del archivo que deseamos generar para nuestro bundle
     path: path.resolve(__dirname, "dist"), //almacenamos nuestro output en un nuevo directorio dentro de nuestro root path
   },
+  plugins: [new MiniCssExtractPlugin({ filename: "main.css" })],
   module: {
     rules: [
       {
         test: /\.scss$/,
         use: [
-          "style-loader", //3. Inject styles into DOM
+          miniCssExtractPlugin.loader, //3. Inject styles into a new file
           "css-loader", //2. Turns css into commonjs
           "sass-loader", //1. Turns sass into css
         ],
